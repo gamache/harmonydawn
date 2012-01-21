41065fab18cd4d077451165a867659539e00bf0d
pete gamache <pete@where.com>
2012-01-21 18:11:37 -0500

    new assets and layout changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,7 @@ HarmonyDawn::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  config.assets.compile = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
